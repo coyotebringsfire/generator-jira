f54ce0f210406a04a36feedcea655aa1820beaf2
CoyoteBringsFire <aumkara@gmail.com>
2015-11-22 19:30:15 -0800

    refactored to use JiraFacade

--- bug/index.js
+++ bug/index.js
@@ -1,126 +1,140 @@
 'use strict';
-var util    = require('util'),
-    yeoman  = require('yeoman-generator'),
-    fs      = require('fs'),
-    path    = require('path'),
-    JiraApi = require('jira').JiraApi,
-    Q       = require('q');
+var util        = require('util'),
+    yeoman      = require('yeoman-generator'),
+    fs          = require('fs'),
+    path        = require('path'),
+    JiraFacade  = require('jira-facade'),
+    Q           = require('q'),
+    util        = require('util');
 
 var JiraGenerator = module.exports = function JiraGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+  var _this = this;
+  yeoman.generators.Base.apply(_this, arguments);
 
-  this.on('end', function () {
-    // this.installDependencies({ skipInstall: options['skip-install'] });
-    this.installDependencies({
-      npm: true,
-      bower: false,
-      skipInstall: options['skip-install'],
-      callback: function() {
-        console.log('Everything is ready!');
-      }
-    });
+  _this.on('end', function () {
+    console.log( util.format("New BUG created %s", _this.new_issue_id) );
   });
 
-  this.readFileAsString = require("html-wiring").readFileAsString;
+  _this.readFileAsString = require("html-wiring").readFileAsString;
   try {
     //var joptions = this.readFileAsString( process.env.YOJIRA );
     var joptions = fs.readFileSync(path.resolve(process.env.YOJIRA), 'utf8');
-    this.jira_options = JSON.parse(joptions);
+    _this.jira_options = JSON.parse(joptions);
   } catch(err) {
-    this.env.error("YOJIRA environment variable must point to a JSON file with yojira options");
+    _this.env.error("YOJIRA environment variable must point to a JSON file with yojira options");
   }
 
-  this.projects = new Q.Promise(function (reject, resolve) {
-    this.jira.listProjects(function(err, projects) {
-      if(err) {
-        reject(err);
-      } else {
-        resolve(JSON.parseprojects);
-      }
-    });
+  _this.jiraFacade = new JiraFacade({
+    protocol: _this.jira_options.protocol || "https",
+    host: _this.jira_options.host || "localhost",
+    port: _this.jira_options.port || 443,
+    username: _this.jira_options.credentials.user,
+    password: _this.jira_options.credentials.password,
+    apiVersion: _this.jira_options.apiVersion || '2.0.alpha1',
   });
-  this.jira = new JiraApi('https', this.jira_options.host, this.jira_options.port, this.jira_options.credentials.user, this.jira_options.credentials.password, '2.0.alpha1');
 };
 
 util.inherits(JiraGenerator, yeoman.generators.Base);
 
 JiraGenerator.prototype.askFor = function askFor() {
-  this.projects.then(function onProjectsReady(projects) {
-      var cb = this.async();
+  var _this = this;
+  var cb = _this.async();
 
-      // have Yeoman greet the user.
-      console.log(this.yeoman);
+  var prompts = [];
 
-      var prompts = [];
-
-      prompts.push({
-        type: 'input', // TODO generate list of projects from jira
-        name: 'project',
-        message: 'What Project are you filing for?',
-        choices: ['FILLER'], 
-        default: 'FILLER'
-      });
-
-      prompts.push({
-        type: 'input',
-        name: 'summary',
-        message: 'Just the elevator pitch (summary)',
-        default: ''
-      });
-
-      prompts.push({
-        type: 'list',
-        name: 'priority',
-        message: 'How important is it?',
-        choices: [1, 2, 3, 4, 5], 
-        default: 1
-      });
-
-      prompts.push({
-        type: 'list',
-        name: 'severity',
-        message: 'How severe is it?',
-        choices: [1, 2, 3, 4, 5], 
-        default: 1
-      });
-
-      prompts.push({
-        type: 'input',
-        name: 'observation',
-        message: 'Tell me what happened',
-        default: ''
-      });
-
-      prompts.push({
-        type: 'input',
-        name: 'expectation',
-        message: 'What did you expect to happen?',
-        default: ''
-      });
-
-      prompts.push({
-        type: 'input',
-        name: 'assignee',
-        message: 'Who should look into this?',
-        default: ''
-      });
-
-      this.prompt(prompts, function (response) {
-        this.project = response.project;
-        this.summary = response.summary;
-        this.priority = response.priority;
-        this.severity = response.severity;
-        this.observation = response.observation;
-        this.expectation = response.expectation;
-        this.assignee = response.assignee;
-        cb();
-      }.bind(this));
+  prompts.push({
+    type: 'input', // TODO generate list of projects from jira
+    name: 'project',
+    message: 'What Project are you filing for?',
+    validate: function validateProject(value) {
+      return value === "" ? "Project Name is required" : true;
+    }
   });
+
+  prompts.push({
+    type: 'input',
+    name: 'summary',
+    message: 'Just the elevator pitch (summary)',
+    validate: function validateProject(value) {
+      return value === "" ? "Summary is required" : true;
+    }
+  });
+
+  prompts.push({
+    type: 'list',
+    name: 'priority',
+    message: 'How important is it?',
+    choices: _this.jira_options.priorities, 
+    default: 1
+  });
+
+  prompts.push({
+    type: 'input',
+    name: 'observation',
+    message: 'Tell me what happened',
+    validate: function validateProject(value) {
+      return value === "" ? "Observation is required" : true;
+    }
+  });
+
+  prompts.push({
+    type: 'input',
+    name: 'expectation',
+    message: 'What did you expect to happen?',
+    validate: function validateProject(value) {
+      return value === "" ? "Expectation is required" : true;
+    }
+  });
+
+  prompts.push({
+    type: 'input',
+    name: 'assignee',
+    message: 'Who should look into this?',
+    validate: function validateProject(value) {
+      return value === "" ? "Assignee is required" : true;
+    }
+  });
+
+  _this.prompt(prompts, function (response) {
+    _this.project = response.project;
+    _this.summary = response.summary;
+    _this.priority = response.priority;
+    _this.observation = response.observation;
+    _this.expectation = response.expectation;
+    _this.assignee = response.assignee;
+    console.dir({
+      project: _this.project,
+      issueType: 'Bug',
+      summary: _this.summary,   
+      description: util.format("Observation:\n%s\nExpectation:\n%s", _this.observation, _this.expectation),
+      assignee: _this.assignee,
+      reporter: _this.jira_options.credentials.user,
+      priority: _this.priority
+    });
+    _this.jiraFacade.createIssue({
+      project: _this.project,
+      issueType: 'Bug',
+      summary: _this.summary,   
+      description: util.format("Observation:\n%s\nExpectation:\n%s", _this.observation, _this.expectation),
+      assignee: _this.assignee,
+      reporter: _this.jira_options.credentials.user,
+      priority: _this.priority
+    }, function(err, issue){
+      if(err) {
+        console.log(util.format("%j", err));
+        cb(err);
+      } else {
+        console.log("createIssue callback %j", issue);
+        _this.new_issue_id = issue && issue.id ? issue.id : undefined;
+        cb();
+      }
+    });
+  }.bind(_this));
 };
 
 JiraGenerator.prototype.app = function projectFiles() {
   // authenticate with jira server and create the new ticket  
-  jira.findIssue(issueNumber, function(error, issue) {
+  /*JiraApi.findIssue(issueNumber, function(error, issue) {
       console.log('Status: ' + issue.fields.status.name);
-  });
+  });*/
 };
